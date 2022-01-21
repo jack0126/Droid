ddf63d1f9675b7255e0f76a46c9a581ad5523485
jack0126 <45025728+jack0126@users.noreply.github.com>
2022-01-21 14:09:08 +0800

    Add files via upload

---
+++ Demo/Activitys/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Jack.Mvvm.Droid;
+
+namespace Demo.Activitys
+{
+    class MainActivity : Activity
+    {
+        private TextBlock MessageBlock;
+        private Button btnGo;
+        public override UserControl OnCreateView()
+        {
+            return new Views.MainActivity();
+        }
+
+        public override void OnViewCreated()
+        {
+            MessageBlock = FindViewByName<TextBlock>("Message");
+            btnGo = FindViewByName<Button>("Go");
+
+            MessageBlock.Text = "Hello, welcome to my world!";
+            btnGo.Click += BtnGo_Click;
+        }
+
+        private void BtnGo_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            StartActivity<TestActivity>();
+        }
+
+        public override void OnResume()
+        {
+
+        }
+
+        public override void OnPause()
+        {
+
+        }
+
+        public override void OnDestroy()
+        {
+
+        }
+
+        public override void OnResult(string source, params object[] args)
+        {
+
+        }
+    }
+}
---
+++ Demo/Activitys/TestActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Jack.Mvvm.Droid;
+
+namespace Demo.Activitys
+{
+    class TestActivity : Activity
+    {
+        private Button btnBack;
+        public override UserControl OnCreateView()
+        {
+            return new Views.TestActivity();
+        }
+
+        public override void OnViewCreated()
+        {
+            btnBack = FindViewByName<Button>("Back");
+            btnBack.Click += BtnBack_Click;
+        }
+
+        private void BtnBack_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Finish();
+        }
+    }
+}
